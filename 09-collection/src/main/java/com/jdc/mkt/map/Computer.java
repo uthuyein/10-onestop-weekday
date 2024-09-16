ce5421733b1b4a173d4c086d12830b0ad588cf1f
uminkhant <uminkhanthu@gmail.com>
2024-09-16 15:53:20 +0630

    collection demo

---
+++ 09-collection/src/main/java/com/jdc/mkt/map/Computer.java
@@ -0,0 +1,18 @@
+package com.jdc.mkt.map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Computer implements Comparable<Computer> {
+
+	private Integer id;
+	private String name;
+	
+	@Override
+	public int compareTo(Computer c) {
+		//return this.name.compareTo(c.name);
+		return this.id > c.id == true ? -1: 1;
+	}
+}
---
+++ 09-collection/src/main/java/com/jdc/mkt/map/MapDemo.java
@@ -0,0 +1,52 @@
+package com.jdc.mkt.map;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapDemo {
+
+	static Map<Integer, Computer> treeMap; 
+	static Map<Computer, Integer> treeMapTwo;
+	
+	public static void main(String[] args) {
+		treeMap = new TreeMap<Integer, Computer>();
+		setDataTreeMap(treeMap);
+		getDataForTreeMap();
+		
+		treeMapTwo = new TreeMap<Computer, Integer>();
+		setDataTreeMapTwo(treeMapTwo);
+		getDataForTreeMapTwo();
+	}
+	
+	static void setDataTreeMapTwo(Map<Computer, Integer> map) {
+		var com = new Computer(3,"Lenovo");
+		
+		map.put( new Computer(2,"Dell"),1);
+		map.put( new Computer(1,"Acer"),2);
+		map.put( com,3);
+		map.put( com,4);
+	}
+	
+	static void setDataTreeMap(Map<Integer, Computer> map) {
+		var com = new Computer(3,"Lenovo");
+		
+		map.put(1, new Computer(2,"Dell"));
+		map.put(2, new Computer(1,"Acer"));
+		map.put(3, com);
+		map.put(4, com);
+	}
+	
+	static void getDataForTreeMapTwo() {
+		for(Map.Entry m: treeMapTwo.entrySet()) {
+			System.out.println(m.getKey()+"\t"+m.getValue());
+		}
+	}
+	
+	static void getDataForTreeMap() {
+		for(Map.Entry m: treeMap.entrySet()) {
+			System.out.println(m.getKey()+"\t"+m.getValue());
+		}
+	}
+}
+
+
