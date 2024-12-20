112b850249561d7dce96fcc0e8164e5e8b85ec7c
uthuyein <uthuyeinlinn@gmail.com>
2024-12-20 11:30:08 +0630

    init proj

---
+++ test_spring_boot_repo/src/main/java/com/jdc/mkt/TestSpringBootRepoApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.mkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootRepoApplication.class, args);
+	}
+
+}
