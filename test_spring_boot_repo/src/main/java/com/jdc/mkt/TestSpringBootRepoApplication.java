65f296000cf61a28d78e916cac5ef812977923f3
uthuyein <uthuyeinlinn@gmail.com>
2024-12-20 11:51:58 +0630

    upd

--- test_spring_boot_repo/src/main/java/com/jdc/mkt/TestSpringBootRepoApplication.java
+++ test_spring_boot_repo/src/main/java/com/jdc/mkt/TestSpringBootRepoApplication.java
@@ -8,6 +8,7 @@ public class TestSpringBootRepoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringBootRepoApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
