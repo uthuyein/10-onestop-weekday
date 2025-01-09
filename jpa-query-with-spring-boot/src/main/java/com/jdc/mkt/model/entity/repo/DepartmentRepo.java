3a3e869e6f0005394b8de6bd85521bbfa0a9bf47
uthuyein <uthuyeinlinn@gmail.com>
2025-01-09 09:44:30 +0630

    update

--- jpa-query-with-spring-boot/src/main/java/com/jdc/mkt/model/entity/repo/DepartmentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jdc.mkt.model.entity.repo;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-import com.jdc.mkt.model.entity.Department;
-
-public interface DepartmentRepo extends JpaRepositoryImplementation<Department, Integer>{
-
-}
