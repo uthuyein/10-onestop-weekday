fff098f8ef3a286cdec37b3be1fdae0e676ced36
uthuyein <uthuyeinlinn@gmail.com>
2025-01-13 11:02:18 +0630

    salary proj

---
+++ 13-spring-boot-salary-proj/src/main/java/com/jdc/mkt/model/entity/Employee.java
@@ -0,0 +1,43 @@
+package com.jdc.mkt.model.entity;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "employee_tbl")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false,length = 45)	
+	private String name;
+	
+	private LocalDate dob;
+	
+	@ColumnDefault("1")
+	private Boolean active;
+	
+	@ManyToOne
+	private Department department;
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+}
